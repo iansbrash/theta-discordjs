1589fe01a985e4a68229b0d05118051156532bf7
Ian Brash <brash@usc.edu>
2021-07-10 16:05:16 -0500

    Start basic functionality of bot

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const { prefix, token } = require('./sensitive/config')
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+// client.cooldowns = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    
+    const command = client.commands.get(commandName);
+
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
